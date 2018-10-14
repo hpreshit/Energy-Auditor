cc184850c1dd9d97c62a4de3baac94e6b244e4d0
Gunj Manseta <gunjmanseta@gmail.com>
2018-10-14 14:16:39 -0600

    clean

--- Sensor Node Source Code/Current-Sensor-BLE-Server/GNU ARM v7.2.1 - Default/application_properties.d
+++
@@ -1,4 +0,0 @@
-application_properties.o: ../application_properties.c \
- C:\Users\Gunj\ Manseta\SimplicityStudio\SampleCodeWorkspace\Current-Sensor-BLE-Server\platform\bootloader\api/application_properties.h
-
-C:\Users\Gunj\ Manseta\SimplicityStudio\SampleCodeWorkspace\Current-Sensor-BLE-Server\platform\bootloader\api/application_properties.h:
